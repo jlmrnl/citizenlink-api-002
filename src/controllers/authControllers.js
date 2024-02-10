d54656c5376a78e636e368ae6ea1da0b5bbf8c39
Jilmer P <77193911+jlmrnl@users.noreply.github.com>
2024-02-10 23:26:04 +0800

    Update controllers

---
+++ src/controllers/authControllers.js
@@ -0,0 +1,38 @@
+const bcrypt = require('bcryptjs');
+const jwt = require('jsonwebtoken');
+const User = require('../models/User');
+
+const registerUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const hashedPassword = await bcrypt.hash(password, 10);
+        const user = new User({ username, password: hashedPassword });
+        await user.save();
+        res.status(201).json({ message: "User registered successfully" });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+const loginUser = async (req, res) => {
+    try {
+        const { username, password } = req.body;
+        const user = await User.findOne({ username });
+        if (!user) {
+            return res.status(401).json({ message: "Invalid username or password" });
+        }
+        const isPasswordMatch = await bcrypt.compare(password, user.password);
+        if (!isPasswordMatch) {
+            return res.status(401).json({ message: "Invalid username or password" });
+        }
+        const token = jwt.sign({ userId: user._id }, 'your_secret_key');
+        res.json({ token });
+    } catch (error) {
+        res.status(500).json({ message: error.message });
+    }
+};
+
+module.exports = {
+    registerUser,
+    loginUser
+};
