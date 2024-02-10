18e04ee21ea2e1e69765a5e6cdafd793f0d4d1cc
Jilmer P <77193911+jlmrnl@users.noreply.github.com>
2024-02-10 22:42:28 +0800

    Update userModels.js

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -1,3 +1,22 @@
 const mongoose = require('mongoose');
 
-const userSchema = 
+const userSchema = mongoose.model({
+     name: {
+       type: String,
+       required: false
+    },
+     userId: {
+       type: int,
+       required: true
+    },
+     password: {
+       type: String,
+       required: true
+    }
+)};
+
+const userSchema = mongoose.schema({
+
+)}
+
+export userSchema
