03524ba0a9b3173d47788bf6cc22876bb94ccad1
Jilmer P <77193911+jlmrnl@users.noreply.github.com>
2024-02-10 22:45:59 +0800

    Update userModels.js

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -4,15 +4,15 @@ const userSchema = mongoose.model({
      name: {
        type: String,
        required: false
-    },
+     },
      userId: {
        type: int,
        required: true
-    },
+     },
      password: {
        type: String,
        required: true
-    }
+     }
 )};
 
 const userModel = mongoose.model('userModel', userSchema);
