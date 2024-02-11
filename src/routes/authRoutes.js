2a68b5f6995a89cfa422a4c3fd1d4bb27ecddbb8
Jilmer P <77193911+jlmrnl@users.noreply.github.com>
2024-02-11 17:27:22 +0800

    added checkAdminRole routes

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -4,5 +4,6 @@ const { registerUser, loginUser } = require('../controllers/authControllers');
 
 router.post('/register', registerUser);
 router.post('/login', loginUser);
+router.get('/userCount, checkAdminRole);
 
 module.exports = router;
