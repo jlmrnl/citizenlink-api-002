b0586774ce67c75f3b355cdbadfd2479f61206d7
Jilmer P <77193911+jlmrnl@users.noreply.github.com>
2024-02-17 09:42:28 +0800

    Update userId as unique

--- src/models/userModels.js
+++ src/models/userModels.js
@@ -7,7 +7,8 @@ const userSchema = new mongoose.Schema({
      },
      userId: {
        type: String,
-       required: true
+       required: true,
+       unique: true
      },
      password: {
        type: String,
