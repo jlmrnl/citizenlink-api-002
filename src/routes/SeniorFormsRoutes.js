b797d5a97d368d8f74e1724e0c42ee384d715d1d
Jilmer Padua <jiis.padua.up@phinamed.com>
2024-02-22 07:26:45 +0800

    implemented authorization middleware to forms routes

--- src/routes/SeniorFormsRoutes.js
+++ src/routes/SeniorFormsRoutes.js
@@ -4,15 +4,16 @@ const multer = require('multer');
 const path = require('path');
 const { submitForm, getAllEntries, getEntryById, updateEntry, deleteEntry } = require('../controllers/SeniorFormsControllers');
 const { configureMulter } = require('../utils/multerHelpers');
+const authenticateUser = require('../middleware/authMiddleware');
 
 // Multer configuration
 const upload = configureMulter();
 
 // Routes
-router.post('/submit', upload.single('picture'), submitForm);
-router.get('/entries', getAllEntries);
-router.get('/entries/:id', getEntryById);
-router.put('/entries/:id', updateEntry);
-router.delete('/entries/:id', deleteEntry);
+router.post('/submit', authenticateUser, upload.single('picture'), submitForm);
+router.get('/entries', authenticateUser, getAllEntries);
+router.get('/entries/:id', authenticateUser, getEntryById);
+router.put('/entries/:id', authenticateUser, updateEntry);
+router.delete('/entries/:id', authenticateUser, deleteEntry);
 
 module.exports = router;
--- src/routes/_4PsFormsRoutes.js
+++ src/routes/_4PsFormsRoutes.js
@@ -1,11 +1,12 @@
 const express = require('express');
 const router = express.Router();
 const { submitForm, getAllForms, getFormById, updateFormById, deleteFormById } = require('../controllers/_4PsFormsControllers');
+const authenticateUser = require('../middleware/authMiddleware');
 
-router.post('/submit', submitForm);
-router.get('/forms', getAllForms);
-router.get('/forms/:id', getFormById);
-router.put('/forms/:id', updateFormById);
-router.delete('/forms/:id', deleteFormById);
+router.post('/submit', authenticateUser, submitForm);
+router.get('/forms', authenticateUser, getAllForms);
+router.get('/forms/:id', authenticateUser, getFormById);
+router.put('/forms/:id', authenticateUser, updateFormById);
+router.delete('/forms/:id', authenticateUser, deleteFormById);
 
 module.exports = router;
