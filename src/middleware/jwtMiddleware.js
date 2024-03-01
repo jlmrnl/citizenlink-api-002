5d753fdf74416d9de6f0bb4f2c8675d6644fa0b6
Jilmer Padua <jiis.padua.up@phinamed.com>
2024-03-02 01:55:48 +0800

    userId extraction fixed

--- src/middleware/jwtMiddleware.js
+++ src/middleware/jwtMiddleware.js
@@ -4,10 +4,10 @@ const extractUserIdFromToken = (req, res, next) => {
   const token = req.headers.authorization && req.headers.authorization.split(' ')[1];
   if (token) {
     jwt.verify(token, process.env.SECRET, (err, decodedToken) => {
-      if (err) {
+      if (err || !decodedToken || !decodedToken.userId) { // Check if decodedToken or userId is undefined
         return res.status(401).json({ message: 'Invalid token' });
       } else {
-        req.userId = decodedToken.user.userId; // Correctly access userId from decoded token
+        req.userId = decodedToken.userId; // Correctly access userId from decoded token
         next();
       }
     });
