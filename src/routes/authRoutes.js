fcdd89983f3b0cb7477152245fb5cd03f76a8a7a
Jilmer Padua <jiis.padua.up@phinamed.com>
2024-03-19 10:34:28 +0800

    lgu change password

--- src/routes/authRoutes.js
+++ src/routes/authRoutes.js
@@ -1,14 +1,19 @@
 const express = require("express");
 const router = express.Router();
+const authenticateUser = require("../middleware/authMiddleware");
 const {
   registerUser,
   loginUser,
   checkAdminRole,
   getAllUsers,
+  changePassword
 } = require("../controllers/adminAuthControllers");
 
 router.post("/register", registerUser);
 router.post("/login", loginUser);
+router.put('/change-password', authenticateUser, changePassword);
+
+
 router.get("/userCount", checkAdminRole);
 router.get("/users", getAllUsers);
 
