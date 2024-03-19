73ab5be30d0e3e7794d4047b3e8c6ae3f077ee2f
Jilmer Padua <jiis.padua.up@phinamed.com>
2024-03-19 11:57:03 +0800

    upload picture fixed

--- src/utils/multerHelpers.js
+++ src/utils/multerHelpers.js
@@ -1,14 +1,16 @@
 const multer = require('multer');
 const fs = require('fs').promises;
 
-function configureMulter() {
+const configureMulter = () => {
   const storage = multer.diskStorage({
     destination: async function (req, file, cb) {
       const uploadPath = 'uploads/';
       try {
         await fs.mkdir(uploadPath, { recursive: true });
+        // If directory creation is successful, call the callback function with null error
         cb(null, uploadPath);
       } catch (error) {
+        // If an error occurs during directory creation, pass the error to the callback function
         cb(error);
       }
     },
@@ -18,7 +20,17 @@ function configureMulter() {
     }
   });
 
-  return multer({ storage: storage });
-}
+  return multer({ 
+    storage: storage,
+    fileFilter: (req, file, cb) => {
+      // Ensure only certain file types are allowed, adjust this as needed
+      if (file.mimetype === 'image/jpeg' || file.mimetype === 'image/png') {
+        cb(null, true);
+      } else {
+        cb(new Error('Invalid file type. Only JPEG and PNG files are allowed.'));
+      }
+    }
+  });
+};
 
 module.exports = { configureMulter };
